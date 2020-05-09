6aa8fea43e91d37432843ff7a7bab9227d4b6590
jagangirisaballa <63942194+jagangirisaballa@users.noreply.github.com>
2020-05-09 23:33:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	df= pd.read_csv("https://github.com/jagangirisaballa/Heroku-Demo/blob/master/data/YoutubeSpamMergeddata.csv", error_bad_lines=False, encoding= 'latin-1')
+	df= pd.read_csv("https://github.com/jagangirisaballa/Heroku-Demo/blob/master/data/YoutubeSpamMergeddata.csv")
 	df_data = df[["CONTENT","CLASS"]]
 	# Features and Labels
 	df_x = df_data['CONTENT']
